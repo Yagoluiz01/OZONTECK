f70fd300b4b701dfd45ba63993e8bb90b382dc1d
Yago Luiz <yagoluiz1012@gmail.com>
2026-06-24 14:16:16 -0300

    secure reports endpoint

--- src/routes/report.routes.js
+++ src/routes/report.routes.js
@@ -1,7 +1,12 @@
 import { Router } from "express";
 import { generateProductsReport } from "../controllers/report.controller.js";
+import { requireAdminAuth } from "../middlewares/auth.middleware.js";
+import { requireMasterAdmin } from "../middlewares/masterAdmin.middleware.js";
 
 const router = Router();
+
+router.use(requireAdminAuth);
+router.use(requireMasterAdmin);
 
 router.get(
   "/products/excel",
